dc96337d01ebad4a230e5ef0e3727d59e45fb166
CASTELLANI <lucas100castellani@gmail.com>
2024-09-09 18:57:49 -0300

    feat: auth added

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request): JsonResponse
+    {
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('MyApp')->plainTextToken;
+            $success['name'] =  $user->name;
+
+            return response()->json($success, 200);
+        } else {
+            return response()->json('Unauthorized.', 403);
+        }
+    }
+}
